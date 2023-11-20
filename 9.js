a64891fb298bf0ec403c98d1933f33f3655a4f07
Muhammad Raffey <muhammadraffey26@gmail.com>
2023-11-20 17:21:10 +0500

    Question 1-17 done

---
+++ 9.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+`Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.`;
+let favNum = 26; // Declaring and Initializing my Fav Num.
+let msg = `My Favorite Number is "${favNum}".`; // Creating msg in a variable
+console.log(msg); // Printing The message
